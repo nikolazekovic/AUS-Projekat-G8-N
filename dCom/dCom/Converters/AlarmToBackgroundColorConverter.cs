6c7450f7b7c34ce456824afa1b2f8aa72f2c4d27
Nikola Zeković <zekovic.pr139.2022@uns.ac.rs>
2026-04-25 22:05:30 +0200

    Implementirane Modbus read/write funkcije za digitalne izlaze i analogne tačke

--- dCom/dCom/Converters/AlarmToBackgroundColorConverter.cs
+++ dCom/dCom/Converters/AlarmToBackgroundColorConverter.cs
@@ -27,7 +27,7 @@ namespace dCom.Converters
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			throw new NotImplementedException();
+			return Binding.DoNothing;
 		}
 	}
 }
--- dCom/dCom/Converters/PointTypeToVisibilityConverter.cs
+++ dCom/dCom/Converters/PointTypeToVisibilityConverter.cs
@@ -28,7 +28,7 @@ namespace dCom.Converters
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			throw new NotImplementedException();
+			return Binding.DoNothing;
 		}
 	}
 }
